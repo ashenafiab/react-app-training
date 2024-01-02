633504940f14b756e07bc625faff42c6b7886095
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-02 20:51:38 +0300

    Javascript Expressions in JSX & ES6 Template Literals

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import { createRoot } from "react-dom/client";
 //which is the same as below
 const root = createRoot(document.getElementById("root"));
 
+const fname = "Ashenafi";
+const lname = "Abebaw";
+const num = 7;
 root.render(
   <div>
-    <h1>My Favorite Foods</h1>
-    <ul
-    // style={{ listStyleType: "decimal" }}
-    >
-      <li>Burger</li>
-      <li>Shekla Tibs</li>
-      <li>Kitfo</li>
-    </ul>
+    <h1>Hello {fname + " " + lname}!</h1>
+    <p>Your Lucky Number is {num}</p>
+    {/* <p>Your Lucky Number is {3+9}</p> */}
+    {/* <p>Your Lucky Number is {Math.random()}</p> */}
+    {/* <p>Your Lucky Number is {Math.floor(Math.random() * 10)}</p> */}
   </div>
 );
