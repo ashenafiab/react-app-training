ddcb9e52b27b488c19bb4cfc3ab336d97c1bfcd5
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-03 00:59:06 +0300

    React Props(properties) Practice

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+      name: "Matt LeBlanc",
+      imgURL:
+        "https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/960w/products/75500/75297/ss3089190_-_photograph_of_matt_leblanc_available_in_4_sizes_framed_or_unframed_buy_now_at_starstills__79880__15946.1394506754.jpg?c=2",
+      phone: "+123 456 789",
+      email: "matt@leblanc.com"
+    },
+    {
+        name: "Lisa Kurdow",
+      imgURL:
+      "https://i.insider.com/5ec036b5f34d056334397275?width=1136&format=jpeg",
+      phone: "+7387384587",
+      email: "lisa@kurdow.com"
+    },
+    {
+      name: "Ashenafi A",
+      imgURL:
+        "https://media.licdn.com/dms/image/D4E03AQFPOFyVUElmVg/profile-displayphoto-shrink_800_800/0/1679924676844?e=2147483647&v=beta&t=CLdJBFlo2a8wj2SB-1jpOhTlKfJJeWn8Nn21EcfE950",
+      phone: "+251000000000",
+      email: "ashu@gmail.com"
+    }
+  ];
+  
+    export default contacts;
