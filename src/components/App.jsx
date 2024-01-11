ca546705d50750606565d8277b7f1220af0675f3
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-11 15:21:39 +0300

    Mapping Data to Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,24 +8,15 @@ function App() {
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media.licdn.com/dms/image/D4E03AQFPOFyVUElmVg/profile-displayphoto-shrink_800_800/0/1679924676844?e=2147483647&v=beta&t=CLdJBFlo2a8wj2SB-1jpOhTlKfJJeWn8Nn21EcfE950" />
-      <Card 
-      name={contacts[0].name}
-        img={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card 
-      name={contacts[1].name}
-        img={contacts[1].imgURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card 
-      name={contacts[2].name}
-        img={contacts[2].imgURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map((contact) => (
+        <Card
+          key={contact.id} // this is a special prop that React will look for and use to identify each unique element amongst its siblings. This way, React will know which elements to re-render when a change occurs.
+          name={contact.name}
+          img={contact.imgURL}
+          phone={contact.phone}
+          email={contact.email}
+        />
+      ))}
     </div>
   );
 }
