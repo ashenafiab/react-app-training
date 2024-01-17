f0770fa7e257e4a26a31434f9e948e7eaf1c1d81
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-17 16:32:02 +0300

    some Javascript Methods in ES6;map,filter,reduce,find,findIndex

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App";
+var numbers = [2, 6, 10, 14, 18];
 
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
+//Map -Create a new array by doing something with each item in an array. 
+const newNumbers1 = numbers.map(num => num * 2);
+console.log(newNumbers1);
 
-//which is the same as below
-const root = createRoot(document.getElementById("root"));
+//Filter - Create a new array by keeping the items that return true. 
+const newNumbers2 = numbers.filter(num => num > 6);
+console.log(newNumbers2);
 
-root.render(
-    <App />
-);
+//Reduce - Accumulate a value by doing something to each item in an array.
+const newNumbers3 = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+console.log(newNumbers3);
+
+//Find - find the first item that matches from an array.
+const newNumbers4 = numbers.find(num => num > 6);
+console.log(newNumbers4);
+
+//FindIndex - find the index of the first item that matches.
+const newNumbers5 = numbers.findIndex(num => num > 6);
+console.log(newNumbers5);
+
+// using these 5 types of methods(or functions) helps us to minimize the effort of writing the code using "foreach + push" method.
