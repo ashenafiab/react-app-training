062ad8092855d423b91ea1a96cc4dea85d64ed3f
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-19 11:56:56 +0300

    Conditional Rendering Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,14 @@
 import React from "react";
-import Login from "./Login";
+import Form from "./Form";
 
-var isLoggedIn = false;
+var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      {/* Ternary Operator */}
-      {isLoggedIn ? <h1>Hello User</h1> : <Login />}
-
-      {/* AND Operator */}
-      {/* {isLoggedIn && <h1>Hello User</h1>} */}
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
 
 export default App;
-
-// the AND operator is used to conditionally render elements. If the first condition is true, the second condition will be evaluated. If the first condition is false, the second condition will be ignored.
-
-// The AND operator is useful when you want to conditionally render an element, but only if the first condition is true.
-
--- src/index.js
+++ src/index.js
@@ -11,3 +11,13 @@ const root = createRoot(document.getElementById("root"));
 root.render(
     <App />
 );
+
+
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+// all challenges are completed 😊
+
