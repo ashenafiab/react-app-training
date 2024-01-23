b9ccc5a9843decdc2214534687391a2599240e07
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-23 22:53:08 +0300

    Event Handling in React

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function App() {
+
+    const [headingText , setHeadingText] = React.useState("Hello");
+    const [isMouseOver , setMouseOver] = React.useState(false);
+
+    function handleClick() {
+        setHeadingText("Submitted");
+    }
+
+    function handleMouseOver() {
+        setMouseOver(true);
+    }
+
+    function handleMouseOut() {
+        setMouseOver(false);
+    }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" id="name" placeholder="What's your name?" autoComplete="off" />
+      <button style={{backgroundColor: isMouseOver ? "black" : "white"}} onClick={handleClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} >Submit</button>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import cars from "./practice";
-
-// Destructure the cars array to get the first item in the array
-const [honda, tesla] = cars;
-
-// Destructure the speedStats object to get the topSpeed property
-// from the first item in the array
-const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
-const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
-
-// Destructure the coloursByPopularity array to get the first item
-// from the first item in the array
-const {coloursByPopularity: [hondaTopColour]} = honda;
-const {coloursByPopularity: [teslaTopColour]} = tesla;
+import App from "./components/App";
 
 // const rootElement = document.getElementById("root");
 // const root = createRoot(rootElement);
@@ -22,22 +9,6 @@ const {coloursByPopularity: [teslaTopColour]} = tesla;
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <table>
-        <tr>
-            <th>Brand</th>
-            <th>Top Speed</th>
-            <th>Top Colour</th>
-        </tr>
-        <tr>
-            <td>{tesla.model}</td>
-            <td>{teslaTopSpeed}</td>
-            <td>{teslaTopColour}</td>
-        </tr>
-        <tr>
-            <td>{honda.model}</td>
-            <td>{hondaTopSpeed}</td>
-            <td>{hondaTopColour}</td>
-        </tr>
-    </table>
+    <App />
 );
 
