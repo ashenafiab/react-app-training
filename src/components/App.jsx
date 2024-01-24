9ad338c956d17e79f6e3145fb5c67bd483a6c8b3
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-24 11:43:34 +0300

    Changing Complex State Practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [fullName, setFullName] = React.useState({
+  const [contact, setContact] = useState({
     fName: "",
     lName: "",
+    email: ""
   });
 
   function handleChange(event) {
-    // const { value, name } = event.target;
-    // the above line of code except that the variable names are changed is the same as the below two lines of code, but in a single line of code using ES6 destructuring concept of JS
-    const newvalue = event.target.value;
-    const inputname = event.target.name;
+    const { name, value } = event.target;
 
-    setFullName((prevValue) => {
-      if (inputname === "fName") {
+    setContact((prevValue) => {
+      if (name === "fName") {
         return {
-          fName: newvalue,
+          fName: value,
           lName: prevValue.lName,
+          email: prevValue.email
         };
-      } else if (inputname === "lName") {
+      } else if (name === "lName") {
         return {
           fName: prevValue.fName,
-          lName: newvalue,
+          lName: value,
+          email: prevValue.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
         };
       }
     });
@@ -30,21 +36,13 @@ function App() {
   return (
     <div className="container">
       <h1>
-        Hello {fullName.fName} {fullName.lName}
+        Hello {contact.fName} {contact.lName}
       </h1>
+      <p>{contact.email}</p>
       <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          placeholder="First Name"
-          value={fullName.fName}
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          placeholder="Last Name"
-          value={fullName.lName}
-        />
+        <input onChange={handleChange} value={contact.fName} name="fName" placeholder="First Name" autoComplete="off" />
+        <input onChange={handleChange} value={contact.lName} name="lName" placeholder="Last Name" autoComplete="off" />
+        <input onChange={handleChange} value={contact.email} name="email" placeholder="Email" autoComplete="off" />
         <button>Submit</button>
       </form>
     </div>
