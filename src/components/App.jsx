256b3d9b8e21437a5eedc352055b661605d22e08
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-24 10:47:47 +0300

    Class Components versus Functional components (i.e., Classes and Hooks)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,12 @@
 import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent.jsx";
 
-function App() {
-  const [name, setName] = React.useState("");
-  const [headingText, setHeadingText] = React.useState("");
-  const [isMouseOver, setMouseOver] = React.useState(false);
-
-  function handleChange(event) {
-    // console.log(event.target.value);
-    setName(event.target.value);
+class App extends React.Component {
+  render() {
+    // return <ClassComponent />; or
+    return <FunctionalComponent />;
   }
-
-  function handleClick(event) {
-    setHeadingText(name);
-    event.preventDefault();
-    // console.log(name);
-  }
-
-  function handleMouseOver() {
-    setMouseOver(true);
-  }
-
-  function handleMouseOut() {
-    setMouseOver(false);
-  }
-
-  return (
-    <div className="container">
-      <h1>Hello {headingText}</h1>
-      <form  onSubmit={handleClick}>
-      <input
-        onChange={handleChange}
-        type="text"
-        id="name"
-        placeholder="What's your name?"
-        autoComplete="off"
-      />
-      <button
-        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-               onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        Submit
-      </button>
-        </form>
-    </div>
-  );
 }
 
 export default App;
