f1492b8213b507e42b4d3ed712ba683cdc60f3bd
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-25 11:17:52 +0300

    Javascript ES6 Spread Operator Practice (i.e., A To-Do List App)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,47 @@
 import React, { useState } from "react";
 
 function App() {
-  const [contact, setContact] = useState({
-    fName: "",
-    lName: "",
-    email: ""
-  });
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
-    const { name, value } = event.target;
-
-    setContact(prevValue => {
-      return {
-        // ...prevValue is a spread operator. It allows you to expand an iterable like an array or object into individual elements.
-        ...prevValue, 
-        [name]: value
-        // [name] is a computed property name. It allows you to use a variable as a key in an object literal property name. 
-      };
-    });
+    const newValue = event.target.value;
+    setItem(newValue);
   }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+    setItem("");
+  }
+
+  // function deleteItem(id){
+  //   setItems(prevItems => {
+  //     return prevItems.filter((item , index) => {
+  //       return index !== id;
+  //     });
+  //   });
+  // }
 
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
-      <p>{contact.email}</p>
-      <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          value={contact.fName}
-          placeholder="First Name" autoComplete="off"
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          value={contact.lName}
-          placeholder="Last Name" autoComplete="off"
-        />
-        <input
-          onChange={handleChange}
-          name="email"
-          value={contact.email}
-          placeholder="Email" autoComplete="off"
-        />
-        <button>Submit</button>
-      </form>
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" name="item" value={item} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem,index) =>
+            <li key={index}>{todoItem}</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
