9a0d056d171ce74242bbc8ef408dea023b01b159
ashenafiab <ashenafiabebaw02@gmail.com>
2024-01-26 10:30:58 +0300

    Managing Component tree (line-through inline styling)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
   const [item, setItem] = useState("");
@@ -16,14 +17,6 @@ function App() {
     setItem("");
   }
 
-  // function deleteItem(id){
-  //   setItems(prevItems => {
-  //     return prevItems.filter((item , index) => {
-  //       return index !== id;
-  //     });
-  //   });
-  // }
-
   return (
     <div className="container">
       <div className="heading">
@@ -37,9 +30,9 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((todoItem,index) =>
-            <li key={index}>{todoItem}</li>
-          )}
+          {items.map((todoItem, index) => (
+            <ToDoItem key={index} text={todoItem} />
+          ))}
         </ul>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -12,8 +12,3 @@ root.render(
     <App />
 );
 
-//CHALLENGE: Make this app work by applying what you've learnt.
-//1. When new text is written into the input, its state should be saved.
-//2. When the add button is pressed, the current data in the input should be
-//added to an array.
-//3. The <ul> should display all the array items as <li>s
